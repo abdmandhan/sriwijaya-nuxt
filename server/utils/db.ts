6fd38c5b8c52bd90701ea3a316cbf935e4adb450
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2026-01-13 19:29:32 +0700

    Refactor Prisma client import to use '@prisma/client' for consistency and alignment with updated package structure.

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../lib/generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
     const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
