ec0ff55f4d072c320964cb23f462885516cd203d
Abdurrahman Ramadhan <abdmandhan@gmail.com>
2026-01-13 20:41:37 +0700

    Update Prisma configuration to utilize `env` for environment variable access and modify seed script to hash a new password for user creation.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,14 @@
 
 import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../lib/generated/prisma/client'
+import { PrismaClient } from '@prisma/client'
 import bcrypt from "bcryptjs"
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 const prisma = new PrismaClient({ adapter })
 
 async function main() {
-    const hashedPassword = await bcrypt.hash('12341234', 10)
+    const hashedPassword = await bcrypt.hash('c0fb369ea89b4baf8a63b3f9962f5e28', 10)
     const alice = await prisma.users.create({
         data: {
             email: 'admin@gmail.com',
